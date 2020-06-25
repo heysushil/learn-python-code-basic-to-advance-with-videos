bdac0f0e0e4c92a5c9abfaaf266fa5cfb4f87745
heysushil <sushil.chaudhary111@gmail.com>
2020-06-25 15:41:26 +0530

    changes

---
+++ 5_boolean_and_operators.py
@@ -0,0 +1,59 @@
+# Hi, agar koi question or query ho to YouTube Channel par comment kar sakte ho otherwise mail me on heykyakaru@gmail.com
+# YouTube Channel Link: https://www.youtube.com/channel/UCphs2JfmIClR62wbyf76HDg
+# New updates ke liye visit
+
+# Also contact for College Project & Project Report
+
+# HEY KYA KARU यूट्यूब चैनल के जरूरी लिंक 
+# 1. Python Basic and Advance ट्यूटोरियल प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QikS9PMYrGZXz1HlE1KZLD3
+# 2. PHP Projects प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QiEj6IPc3lkjz1wR4w9RM6B
+# 3. GitHubट्यूटोरियल प्लेलिस्ट  https://www.youtube.com/watch?v=LUyVs2MTlTM&list=PLK6wiPavf7Qjydpc5v-hdIoqCx2V19pHP
+# 4. Python Project https://www.youtube.com/watch?v=3lrbbB38zpU&list=PLK6wiPavf7Qj-NLJhbkxw9QfonweHafcN
+# 5. Tips and Trick for Programming: https://www.youtube.com/watch?v=vPL6ODrfcwI&list=PLK6wiPavf7QiVLYXrC2TW_fdcZp57MgMB
+
+'''
+Topics: Boolean Number System
+'''
+# Boolean Values: True & False
+print(5 > 6)
+if 5 < 6:
+    print('Yes its right')
+else:
+    print('No itr not right')
+# Evaluate Values and Variables bool()
+print(bool('adf'))
+print(bool(0))
+print(bool())
+
+def check():
+    message = 'Hello check method'
+    return True
+
+if check() == True:
+    print('Yes your message get by check method')
+
+# Most Values are True: non-empty values
+# Some Values are False: false, empty,0
+# Functions can Return a Boolean
+
+
+'''
+Topics: Operators
+
+Arithmetic operators: +, -, *, /, %, **, //
+Assignment operators: =, +=, -=, *=, /=, %=, **=, //=,
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+'''
+
+
+'''
+Question:
+
+1. user se input lena hai aur agar user kewal digti enter kar raha hai to ture message otherwise false message show karana hai?
+
+
+'''
