0067bd29f24ee20c521bc2017baf10968292502c
heysushil <sushil.chaudhary111@gmail.com>
2020-10-25 16:24:30 +0530

    delete database

--- Database/12.delete_database.py
+++ Database/12.delete_database.py
@@ -1,20 +1,76 @@
-import installing_and_connecting_db as con
-mycursor = con.mydb.cursor()
+'''
+Ending Topics:
 
-def createTable():
-    tablecolumn = int(input('\nEnter total number of comumb: '))
-    usertable = []
-    i = 0
-    while i < tablecolumn:
-        columnname = input('Enter colmun {} name: '.format(i+1))
-        usertable.append(columnname)
-        i += 1
-    print('\nColumn names: ', usertable)
-    exit()
-    mycursor.execute('CREATE TABLE IF NOT EXISTS demotable(sr INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, goods_name VARCHAR(250), price INT(8), dates VARCHAR(12), person VARCHAR(100))')
+1. Delete Database
+2. Create function and class to use them
+3. What next
+    1. Few more important libarys
+    2. Excercise code basic to advance
+    3. Project discussion and work flow
+    4. Interview Prepration:
+        1. Resume
+        2. Platforms
+        3. Right call
+        4. Right Job
+'''
 
-    print('\nList of tables: ')
-    for t in mycursor.execute('SHOW TABLES'):
-        print(t)
+# create connection
+import mysql.connector as con
 
-createTable()
+mydb = con.connect(host='localhost', user='root', password='')
+
+mycursor = mydb.cursor()
+
+# delete database
+# mycursor.execute('CREATE DATABASE heysushil_demo')
+# mycursor.execute('DROP DATABASE heysushil_demo')
+
+# create funciton for deleting database
+
+class DBClass:
+    def deleteDatabase(self):
+        mycursor.execute('SHOW DATABASES')
+        for db in mycursor:
+            print('Database : ', db[0])
+
+        # DELETE DATABASE
+        dbname = input('Enter database name jo delete karna hai: ')
+        mycursor.execute('DROP DATABASE IF EXISTS ' + dbname)
+
+# call deelteDatabse
+myobj = DBClass()
+myobj.deleteDatabase()
+# deleteDatabase()
+
+'''
+Excercise:
+
+1. student name se table bano iske andar:
+    Following fields de sakte hain:
+        no
+        roll_number
+        name
+        fathername
+        mothername
+        class
+        mobile
+        email
+        registration_date
+
+2. Marks name se table bano:
+    followind files
+        no
+        roll_number
+        hindi
+        english
+        sci
+        math
+
+3. At last table bane ke bad kuch function bana hain aur unka work kuch ye hoga:
+
+    1. new student registration
+    2. add student marks
+    3. show result
+        1. number abhi update 
+    4. delete studnet with student detaila and marks also
+'''
